f7e0ae9cb13a06365cf230cf272828f1949e22ed
吴万杰 <wuwanjie@datagrand.com>
2025-03-11 00:21:19 +0800

    fix bug

--- app/main.py
+++ app/main.py
@@ -8,7 +8,11 @@ from src.routers.graph_manager import GRAPH_MANAGER
 from src.settings.load_env import env
 
 # 创建 FastAPI 实例
-APP = FastAPI()
+APP = FastAPI(
+    title="族谱关系维护API",
+    description="""所有后端API都会基于该文档进行管理，在正式具体接口功能之前，都会预先定义好输入输出接口。\n相关文档:
+    [原型说明文档](https://v0.dev/chat/family-tree-55Jlpju3SKn?b=b_UbNoCuL2O0X)""",
+)
 
 # 注册路由
 # APP.include_router(HEALTH_ROUTER)
